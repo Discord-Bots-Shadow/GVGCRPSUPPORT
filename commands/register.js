dd98c557a17f55fd0e7bcc5775316ec511d57b5d
ExbotsSites <ita.exbot@gmail.com>
2025-09-19 16:59:48 +0200

    Update register.js

--- commands/register.js
+++ commands/register.js
@@ -1,1 +1,29 @@
+import { SlashCommandBuilder } from 'discord.js';
+import fs from 'fs';
 
+export default {
+  data: new SlashCommandBuilder()
+    .setName('register') // nome comando rimane uguale
+    .setDescription('Inregistreaza o masina') // descrizione può avere caratteri romeni
+    .addStringOption(opt => opt.setName('marca').setDescription('Marca masinii').setRequired(true))
+    .addStringOption(opt => opt.setName('model').setDescription('Modelul masinii').setRequired(true))
+    .addStringOption(opt => opt.setName('culoare').setDescription('Culoarea masinii').setRequired(true))
+    .addStringOption(opt => opt.setName('nr_de_inmatriculare').setDescription('Numarul de inmatriculare').setRequired(true)),
+
+  async execute(interaction) {
+    const car = {
+      user: interaction.user.id,
+      make: interaction.options.getString('marca'),
+      model: interaction.options.getString('model'),
+      color: interaction.options.getString('culoare'),
+      plate: interaction.options.getString('nr_de_inmatriculare')
+    };
+
+    // Legge e scrive il database
+    const db = JSON.parse(fs.readFileSync('database.json'));
+    db.push(car);
+    fs.writeFileSync('database.json', JSON.stringify(db, null, 2));
+
+    await interaction.reply(`✅ Masina inregistrata: ${car.make}, ${car.model}, ${car.plate}`);
+  }
+};
