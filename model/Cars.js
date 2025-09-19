f23d69b4424e09f38979771daddda930728c33a0
ExbotsSites <ita.exbot@gmail.com>
2025-09-19 22:10:22 +0200

    Update Cars.js

--- model/Cars.js
+++ model/Cars.js
@@ -1,11 +1,11 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
 const carSchema = new mongoose.Schema({
-  user: { type: String, required: true },     // ID dell’utente Discord
-  make: { type: String, required: true },     // Marca auto
-  model: { type: String, required: true },    // Modello auto
-  color: { type: String, required: true },    // Colore auto
-  plate: { type: String, required: true }     // Targa auto
+  user: { type: String, required: true },
+  make: { type: String, required: true },
+  model: { type: String, required: true },
+  color: { type: String, required: true },
+  plate: { type: String, required: true }
 });
 
-export default mongoose.model("Car", carSchema);
+export default mongoose.model('Car', carSchema);
