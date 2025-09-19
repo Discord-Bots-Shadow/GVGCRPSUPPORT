21574e7f544c44d4ff589e5756d92a272ad3090e
ExbotsSites <ita.exbot@gmail.com>
2025-09-19 22:09:38 +0200

    Update register.js

--- commands/register.js
+++ commands/register.js
@@ -1,28 +1,31 @@
 import { SlashCommandBuilder } from 'discord.js';
-import fs from 'fs';
+import Car from '../models/Car.js';
 
 export default {
   data: new SlashCommandBuilder()
     .setName('register')
-    .setDescription('Register a car')
+    .setDescription('Register a new car')
     .addStringOption(opt => opt.setName('make').setDescription('Car make').setRequired(true))
     .addStringOption(opt => opt.setName('model').setDescription('Car model').setRequired(true))
     .addStringOption(opt => opt.setName('color').setDescription('Car color').setRequired(true))
     .addStringOption(opt => opt.setName('plate').setDescription('Car plate').setRequired(true)),
 
   async execute(interaction) {
-    const car = {
-      user: interaction.user.id,
-      make: interaction.options.getString('make'),
-      model: interaction.options.getString('model'),
-      color: interaction.options.getString('color'),
-      plate: interaction.options.getString('plate')
-    };
+    try {
+      const newCar = new Car({
+        user: interaction.user.id,
+        make: interaction.options.getString('make'),
+        model: interaction.options.getString('model'),
+        color: interaction.options.getString('color'),
+        plate: interaction.options.getString('plate')
+      });
 
-    const db = JSON.parse(fs.readFileSync('database.json'));
-    db.push(car);
-    fs.writeFileSync('database.json', JSON.stringify(db, null, 2));
+      await newCar.save();
 
-    await interaction.reply(`✅ Car registered: ${car.make}, ${car.model}, ${car.plate}`);
+      await interaction.reply(`✅ Car registered: ${newCar.make} ${newCar.model} (${newCar.plate})`);
+    } catch (err) {
+      console.error(err);
+      await interaction.reply({ content: '❌ Error registering the car.', ephemeral: true });
+    }
   }
 };
