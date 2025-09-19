00d6aa1dc78576fce14059e06b7bfd2235fb1f13
ExbotsSites <ita.exbot@gmail.com>
2025-09-19 21:11:52 +0200

    Update unregister.js

--- commands/unregister.js
+++ commands/unregister.js
@@ -4,25 +4,19 @@ import fs from 'fs';
 export default {
   data: new SlashCommandBuilder()
     .setName('unregister')
-    .setDescription('Remove a car')
-    .addStringOption(opt => opt.setName('make').setDescription('Car make').setRequired(true))
-    .addStringOption(opt => opt.setName('model').setDescription('Car model').setRequired(true))
-    .addStringOption(opt => opt.setName('plate').setDescription('Car plate').setRequired(true)),
+    .setDescription('Rimuovi una macchina registrata')
+    .addStringOption(opt => opt.setName('plate').setDescription('Targa').setRequired(true)),
 
   async execute(interaction) {
-    const make = interaction.options.getString('make');
-    const model = interaction.options.getString('model');
     const plate = interaction.options.getString('plate');
 
     let db = JSON.parse(fs.readFileSync('database.json'));
-    const newDb = db.filter(car =>
-      !(car.user === interaction.user.id && car.make === make && car.model === model && car.plate === plate)
-    );
+    const newDb = db.filter(car => !(car.user === interaction.user.id && car.plate === plate));
 
     fs.writeFileSync('database.json', JSON.stringify(newDb, null, 2));
 
     await interaction.reply({
-      content: `✅ Car with plate **${plate}** removed successfully.`,
+      content: `✅ La macchina con targa **${plate}** è stata rimossa.`,
       ephemeral: true
     });
   }
