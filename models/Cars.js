9a0671cad0dd3bed76fd9933e642b6a5f098cf64
ExbotsSites <ita.exbot@gmail.com>
2025-09-20 07:52:09 +0200

    Update Cars.js

--- models/Cars.js
+++ models/Cars.js
@@ -1,1 +1,27 @@
+import mongoose from "mongoose";
 
+const carSchema = new mongoose.Schema({
+  user: {
+    type: String,
+    required: true
+  },
+  make: {
+    type: String,
+    required: true
+  },
+  model: {
+    type: String,
+    required: true
+  },
+  color: {
+    type: String,
+    required: true
+  },
+  plate: {
+    type: String,
+    required: true,
+    unique: true
+  }
+});
+
+export default mongoose.model("Car", carSchema);
