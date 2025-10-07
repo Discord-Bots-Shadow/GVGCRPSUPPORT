1711eb4cdb4f561fc3d3b94e8ff43c2347757323
ExbotsSites <ita.exbot@gmail.com>
2025-10-07 19:15:18 +0200

    Delete models/GuildConfig.js

--- models/GuildConfig.js
+++
@@ -1,9 +0,0 @@
-import mongoose from "mongoose";
-
-const guildConfigSchema = new mongoose.Schema({
-  guildId: { type: String, required: true, unique: true },
-  hostRoleId: { type: String, required: false },
-  adminRoleId: { type: String, required: false },
-});
-
-export default mongoose.model("GuildConfig", guildConfigSchema);
