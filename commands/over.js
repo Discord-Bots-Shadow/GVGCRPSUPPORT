19c5fdc7dc95ac3d2a85de4c6845377d75f676c4
ExbotsSites <ita.exbot@gmail.com>
2025-10-07 19:03:18 +0200

    Update over.js

--- commands/over.js
+++ commands/over.js
@@ -1,25 +1,24 @@
 import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
-import GuildConfig from "../models/GuildConfig.js";
 
 export default {
   data: new SlashCommandBuilder()
     .setName("over")
     .setDescription("Announce that the session is over.")
     .addStringOption(option =>
-      option.setName("note").setDescription("Add an optional note.").setRequired(false)
-    ),
+      option.setName("note")
+        .setDescription("Add an optional note.")
+        .setRequired(false)),
 
   async execute(interaction) {
     const note = interaction.options.getString("note") || "No additional notes provided.";
+    const sessionHostRoleId = "1416802953146400840";
 
-    const config = await GuildConfig.findOne({ guildId: interaction.guild.id });
-    const hostRoleId = config?.hostRoleId;
-
-    if (!hostRoleId)
-      return interaction.reply({ content: "⚙️ Please run `/setup` first to configure host role.", ephemeral: true });
-
-    if (!interaction.member.roles.cache.has(hostRoleId))
-      return interaction.reply({ content: "🚫 You must have the **Host** role to use this command.", ephemeral: true });
+    if (!interaction.member.roles.cache.has(sessionHostRoleId)) {
+      return await interaction.reply({
+        content: "🚫 You must have the **Session Host** role to use this command.",
+        ephemeral: true,
+      });
+    }
 
     const embed = new EmbedBuilder()
       .setTitle("🔴 Session Over")
