0a8b919dfd686077504d36e03f73f099d8674cf6
ExbotsSites <ita.exbot@gmail.com>
2025-10-07 15:18:38 +0200

    Update release.js

--- commands/release.js
+++ commands/release.js
@@ -1,39 +1,39 @@
-import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+import { SlashCommandBuilder, EmbedBuilder } from "discord.js";
+import GuildConfig from "../models/GuildConfig.js";
 
 export default {
   data: new SlashCommandBuilder()
-    .setName('release')
-    .setDescription('Announce that the session is starting.')
+    .setName("release")
+    .setDescription("Announce that the session is starting.")
     .addUserOption(option =>
-      option.setName('host')
-        .setDescription('Tag the host.')
-        .setRequired(true))
+      option.setName("host").setDescription("Tag the host.").setRequired(true)
+    )
     .addStringOption(option =>
-      option.setName('link')
-        .setDescription('Session link (e.g., server link).')
-        .setRequired(true)),
+      option.setName("link").setDescription("Session link.").setRequired(true)
+    ),
 
   async execute(interaction) {
-    const host = interaction.options.getUser('host');
-    const link = interaction.options.getString('link');
-    const sessionHostRoleId = '1416802953146400840';
+    const host = interaction.options.getUser("host");
+    const link = interaction.options.getString("link");
 
-    if (!interaction.member.roles.cache.has(sessionHostRoleId)) {
-      return await interaction.reply({
-        content: '🚫 You must have the **Session Host** role to use this command.',
-        ephemeral: true
-      });
-    }
+    const config = await GuildConfig.findOne({ guildId: interaction.guild.id });
+    const hostRoleId = config?.hostRoleId;
+
+    if (!hostRoleId)
+      return interaction.reply({ content: "⚙️ Please run `/setup` first to configure host role.", ephemeral: true });
+
+    if (!interaction.member.roles.cache.has(hostRoleId))
+      return interaction.reply({ content: "🚫 You must have the **Host** role to use this command.", ephemeral: true });
 
     const embed = new EmbedBuilder()
-      .setTitle('🚗 Session Starting')
+      .setTitle("🚗 Session Starting")
       .addFields(
-        { name: '👤 Host', value: `${host}`, inline: true },
-        { name: '🔗 Link', value: `${link}`, inline: true }
+        { name: "👤 Host", value: `${host}`, inline: true },
+        { name: "🔗 Link", value: `${link}`, inline: true }
       )
-      .setColor('Yellow')
+      .setColor("Yellow")
       .setTimestamp();
 
-    await interaction.reply({ content: '@everyone', embeds: [embed] });
-  }
+    await interaction.reply({ content: "@everyone", embeds: [embed] });
+  },
 };
